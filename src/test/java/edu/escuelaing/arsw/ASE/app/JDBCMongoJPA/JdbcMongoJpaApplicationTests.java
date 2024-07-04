05f0ecddf8a9373bde1d8f16bb9e1646bfab2882
Johannbulls <johannpez99@gmail.com>
2024-07-04 11:50:40 -0500

    MongoDB

---
+++ src/test/java/edu/escuelaing/arsw/ASE/app/JDBCMongoJPA/JdbcMongoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.ASE.app.JDBCMongoJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcMongoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
